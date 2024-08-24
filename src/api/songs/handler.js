631c9dd5413d6d31eb95066e87156dc5bb0f428c
Rivaldo Juliano <vlado11jr@gmail.com>
2024-08-24 10:54:47 +0700

    feat: update song by id

--- src/api/songs/handler.js
+++ src/api/songs/handler.js
@@ -6,6 +6,7 @@ class SongsHandler {
     this.postSongHandler = this.postSongHandler.bind(this);
     this.getSongsHandler = this.getSongsHandler.bind(this);
     this.getSongByIdHandler = this.getSongByIdHandler.bind(this);
+    this.putSongByIdHandler = this.putSongByIdHandler.bind(this);
   }
 
   async postSongHandler(request, h) {
@@ -46,6 +47,19 @@ class SongsHandler {
       data: {
         song
       }
+    };
+  }
+
+  async putSongByIdHandler(request) {
+    this._validator.validateSongsPayload(request.payload);
+
+    const { id } = request.params;
+
+    await this._service.editSongById(id, request.payload);
+
+    return {
+      status: 'success',
+      message: 'Song successfully updated'
     };
   }
 }
--- src/services/postgres/SongsService.js
+++ src/services/postgres/SongsService.js
@@ -46,6 +46,19 @@ class SongsService {
 
     return result.rows.map(mapDBToModel)[0];
   }
+
+  async editSongById(id, { title, year, genre, performer, duration, albumId }) {
+    const query = {
+      text: 'UPDATE songs SET title = $1, year = $2, genre = $3, performer = $4, duration = $5, album_id = $6 WHERE id = $7 RETURNING id',
+      values: [title, year, genre, performer, duration, albumId, id]
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Failed to updated song. ID not found');
+    }
+  }
 }
 
 module.exports = SongsService;
