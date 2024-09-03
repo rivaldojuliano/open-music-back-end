22bdf3bf8760101d3df0bf12a03d85ec10c573b4
Rivaldo Juliano <vlado11jr@gmail.com>
2024-09-03 17:59:12 +0700

    feat: user registration implemented

--- src/server.js
+++ src/server.js
@@ -1,16 +1,28 @@
 require('dotenv').config();
 const Hapi = require('@hapi/hapi');
+
+// albums
 const albums = require('./api/albums');
 const AlbumsService = require('./services/postgres/AlbumsService');
 const AlbumsValidator = require('./validator/albums');
-const ClientError = require('./exceptions/ClientError');
-const SongsValidator = require('./validator/songs');
+
+// songs
 const songs = require('./api/songs');
 const SongsService = require('./services/postgres/SongsService');
+const SongsValidator = require('./validator/songs');
+
+// users
+const users = require('./api/users');
+const UsersService = require('./services/postgres/UsersService');
+const UsersValidator = require('./validator/users');
+
+const ClientError = require('./exceptions/ClientError');
+
 
 const init = async () => {
   const albumsService = new AlbumsService();
   const songsService = new SongsService();
+  const usersService = new UsersService();
 
   const server = Hapi.server({
     // eslint-disable-next-line no-undef
@@ -37,6 +49,13 @@ const init = async () => {
       options: {
         service: songsService,
         validator: SongsValidator
+      }
+    },
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator
       }
     }
   ]);
