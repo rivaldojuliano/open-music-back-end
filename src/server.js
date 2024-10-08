58d29273c4a0f7cf78a6da536edcb48b6d1be2b3
Rivaldo Juliano <vlado11jr@gmail.com>
2024-10-08 18:12:23 +0700

    feat: export playlist with rabbitmq

--- src/server.js
+++ src/server.js
@@ -34,6 +34,11 @@ const collaborations = require('./api/collaborations');
 const CollaborationsService = require('./services/postgres/CollaborationsService');
 const CollaborationsValidator = require('./validator/collaborations');
 
+// exports
+const _exports = require('./api/exports');
+const ProducerService = require('./services/rabbitmq/ProducerService');
+const ExportsValidator = require('./validator/exports');
+
 const ClientError = require('./exceptions/ClientError');
 
 
@@ -129,6 +134,14 @@ const init = async () => {
         playlistsService,
         usersService,
         validator: CollaborationsValidator
+      }
+    },
+    {
+      plugin: _exports,
+      options: {
+        ProducerService,
+        playlistsService,
+        validator: ExportsValidator
       }
     }
   ]);
---
+++ src/services/rabbitmq/ProducerService.js
@@ -0,0 +1,22 @@
+const  amqp = require('amqplib');
+
+const ProducerService = {
+  sendMessage: async (queue, message)=> {
+    // eslint-disable-next-line no-undef
+    const connection = await amqp.connect(process.env.RABBITMQ_SERVER);
+    const channel = await connection.createChannel();
+
+    await channel.assertQueue(queue, {
+      durable: true
+    });
+
+    // eslint-disable-next-line no-undef
+    await channel.sendToQueue(queue, Buffer.from(message));
+
+    setTimeout(() => {
+      connection.close();
+    }, 1000);
+  }
+};
+
+module.exports = ProducerService;
